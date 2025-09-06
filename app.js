41d7cac3ea045dd09d213f980f196465e6b3df62
Calet Devia <calebtius@gmail.com>
2025-09-06 15:16:07 -0500

    Practice the type of consoles.

--- app.js
+++ app.js
@@ -1,5 +1,70 @@
-console.log("Omg bro!");
-const a = 123;
-let b = 23;
+/* Practice types of consoles */
 
-c = 230; // No sabia que se podia esto
+/* Log, Warn and Error */
+const receiveNumber = (num = 1) => {
+  if (num > 0) console.log(num);
+  else if (num === 0) console.warn(num);
+  else console.error(num);
+};
+
+receiveNumber(3);
+receiveNumber(0);
+receiveNumber(-10);
+
+/* Table */
+/* Useful for objects, arrays and arrays of objects */
+
+const users = [
+  {
+    id: crypto.randomUUID(),
+    name: "Calet Devia",
+    age: "23",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Ronald Devia",
+    age: "20",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Jeferson Bermudez",
+    age: "20",
+  },
+];
+
+console.table(users);
+
+const greeting = { number1: "Hi", number2: "Hello", number3: "Yo!" };
+
+console.table(greeting);
+
+const fruits = ["🍎", "🍊", "🍐"];
+
+console.table(fruits);
+
+/* Group and GroupEnd */
+/* Organized your Logs, Tables, etc in Groups */
+
+console.group("Vegetables");
+console.log("Potato");
+console.log("Tomato");
+console.log("Corn");
+console.groupEnd();
+console.group("Fruits");
+console.log("Guava");
+console.log("Tangerine");
+console.log("Pear");
+console.groupEnd();
+console.log("End of the line");
+
+/* Count */
+/* Is to count count the number of the function was call */
+const login = () => {
+  console.count();
+  console.warn("Account Joined!");
+};
+
+login();
+login();
+login();
+login();
